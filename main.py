e8931264084987fee6929bfad39eaee907cc69b0
kyletamayo <kyle_tamayo@startmail.com>
2024-03-25 10:09:53 -0400

    added character count

--- main.py
+++ main.py
@@ -7,10 +7,30 @@ def read_file(file_path) -> str:
     
     return content
 
+def num_words(file_path) -> int:
+    content = read_file(file_path)
+    count = 0
+
+    content = content.split()
+    for word in content:
+        count += 1
+    
+    return count
+
+def character_count(file_path) -> dict:
+    content = read_file(file_path)
+    char_count = {}
+
+    for c in content.lower():
+        char_count[c] = char_count[c] + 1 if c in char_count else 1
+
+    return(char_count)
+
 def main() -> int:
     file_path = os.path.join('books', 'frankenstein.txt')
 
-    print(read_file(file_path))
+    print(f"Number of words: {num_words(file_path)}")
+    print(character_count(file_path))
     
     return 0
 
